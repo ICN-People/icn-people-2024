dfa949ab098cec4ba81622a5366fdd31b0554f89
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2024-08-11 01:18:08 +0900

    first

---
+++ src/main/java/com/example/demo/IcnpeopleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcnpeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcnpeopleApplication.class, args);
+	}
+
+}
